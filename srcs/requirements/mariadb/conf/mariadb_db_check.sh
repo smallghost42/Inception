de98245c0621001c6d981f9c6620164609d29763
smallghost42 <fenoherymamindrainy@gmail.com>
2025-08-30 15:49:30 +0300

    inception

--- srcs/requirements/mariadb/conf/mariadb_db_check.sh
+++ srcs/requirements/mariadb/conf/mariadb_db_check.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+##!/bin/sh
+#
+#mariadb #check db
+#if ! [ $? -eq 0 ]; then
+#  mariadb-install-db --user=$USER_NAME --datadir=/app/data
+#  mysql -u root -p $DB_ROOT_PASS
+#  CREATE DATABASE $DB_NAME
+#  CREATE USER $ADMIN_NAME@$HOST IDENTIFIED BY $ADMIN_PASS
+#  CREATE USER $USER_NAME@$HOST IDENTIFIED BY $USER_PASS
+#  GRANT ALL PRIVILEGES ON $DB_NAME.* TO $ADMIN_NAME@$HOST
+#  GRANT ALL PRIVILEGES ON $DB_NAME.* TO $USER_NAME@$HOST
+#  FLUSH PRIVILEGES
+#fi
+#
+#rc-service mariadb start
+#mariadb-secure-installation
+#rc-update add mariadb default
 
-mariadb
-if ! [ $? -eq 0 ]; then
-  mariadb-install-db --user=ferafano --datadir=/app/data
-  mysql -u root -p ""
-  CREATE DATABASE wp_inception
-  CREATE USER 'ferafano' IDENTIFIED BY 'Admin_pass'
-  CREATE USER 'small' IDENTIFIED BY 'User_pass'
-  GRANT ALL PRIVILEGES ON wp_inception.* TO 'ferafano'@'localhost'
-  FLUSH PRIVILEGES
-fi
-
-rc-service mariadb start
-mariadb-secure-installation
-rc-update add mariadb default
+#check db volume if exist if not create new
--- srcs/requirements/wordpress/conf/wordpress_db_check.sh
+++ srcs/requirements/wordpress/conf/wordpress_db_check.sh
@@ -3,7 +3,7 @@
 wp core version
 if ! [ $? -eq 0 ]; then
   wp core download
-  wp config create --dbname=wp_inceptiondb --dbuser=ferafano --dbpass=small --dbhost=localhost
+  wp config create --dbname=$DB_NAME --dbuser=$USER_NAME --dbpass=$DB_PASS --dbhost=$HOST
   wp db create
-  wp core install --url=ferafano.42.fr --admin_user=ferafano --admin_password=small
+  wp core install --url=$URL --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASS
 fi
