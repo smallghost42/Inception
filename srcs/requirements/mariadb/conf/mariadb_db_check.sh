c237903b7a58d043a8b8b20b0c65d6f1b42e9a74
Fenohery Mamindrainy Rafanomezana <fenoherymamindrainy@gmail.com>
2025-09-09 10:39:13 +0300

    handle script path

--- srcs/requirements/mariadb/conf/mariadb_db_check.sh
+++ srcs/requirements/mariadb/conf/mariadb_db_check.sh
@@ -1,19 +1,18 @@
-##!/bin/sh
-#
-#mariadb #check db
-#if ! [ $? -eq 0 ]; then
-#  mariadb-install-db --user=$USER_NAME --datadir=/app/data
-#  mysql -u root -p $DB_ROOT_PASS
-#  CREATE DATABASE $DB_NAME
-#  CREATE USER $ADMIN_NAME@$HOST IDENTIFIED BY $ADMIN_PASS
-#  CREATE USER $USER_NAME@$HOST IDENTIFIED BY $USER_PASS
-#  GRANT ALL PRIVILEGES ON $DB_NAME.* TO $ADMIN_NAME@$HOST
-#  GRANT ALL PRIVILEGES ON $DB_NAME.* TO $USER_NAME@$HOST
-#  FLUSH PRIVILEGES
-#fi
-#
-#rc-service mariadb start
-#mariadb-secure-installation
-#rc-update add mariadb default
+#!/bin/sh
+
+# mariadbd #check db
+# if ! [ $? -eq 0 ]; then
+mariadb-install-db --user=$USER_NAME --datadir=/app/data
+mariadbd
+CREATE DATABASE $DB_NAME
+CREATE USER $ADMIN_NAME@$HOST IDENTIFIED BY $ADMIN_PASS_FILE
+CREATE USER $USER_NAME@$HOST IDENTIFIED BY $USER_PASS_FILE
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO $ADMIN_NAME@$HOST
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO $USER_NAME@$HOST
+FLUSH PRIVILEGES
+# fi
+
+rc-service mariadb start
+rc-update add mariadb default
 
 #check db volume if exist if not create new
--- srcs/requirements/wordpress/conf/wordpress_db_check.sh
+++ srcs/requirements/wordpress/conf/wordpress_db_check.sh
@@ -3,7 +3,7 @@
 wp core version
 if ! [ $? -eq 0 ]; then
   wp core download
-  wp config create --dbname=$DB_NAME --dbuser=$USER_NAME --dbpass=$DB_PASS --dbhost=$HOST
+  wp config create --dbname=$DB_NAME --dbuser=$USER_NAME --dbpass=$DB_PASS_FILE --dbhost=$HOST
   wp db create
-  wp core install --url=$URL --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASS
+  wp core install --url=$URL --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASS_FILE
 fi
