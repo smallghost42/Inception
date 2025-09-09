b149b1b0b50bd1bf57038e0eaf918bc5fcfed836
Fenohery Mamindrainy Rafanomezana <fenoherymamindrainy@gmail.com>
2025-09-09 11:11:09 +0300

    handle script mariadb

--- srcs/requirements/mariadb/conf/mariadb_db_check.sh
+++ srcs/requirements/mariadb/conf/mariadb_db_check.sh
@@ -1,18 +1,48 @@
 #!/bin/sh
 
-# mariadbd #check db
-# if ! [ $? -eq 0 ]; then
-mariadb-install-db --user=$USER_NAME --datadir=/app/data
-mariadbd
-CREATE DATABASE $DB_NAME
-CREATE USER $ADMIN_NAME@$HOST IDENTIFIED BY $ADMIN_PASS_FILE
-CREATE USER $USER_NAME@$HOST IDENTIFIED BY $USER_PASS_FILE
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO $ADMIN_NAME@$HOST
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO $USER_NAME@$HOST
-FLUSH PRIVILEGES
-# fi
+if [ ! -d "mariadb" ]; then
+  mariadb-install-db --user=$USER_NAME --datadir=/app/data
 
-rc-service mariadb start
-rc-update add mariadb default
+  #skip-networking and --skip-log-bin flags are for security during setup.
+  nohup mariadbd --datadir=$DB_DATA_DIR --user=$USER_NAME \
+    --skip-networking --skip-log-bin >/dev/null 2>&1 &
+  # Store the process ID to kill it later.
+  MARIADB_PID=$!
+  # Wait for the MariaDB daemon to be ready.
+  # We poll the server status until it's accepting connections.
+  for i in {30..0}; do
+    if mariadb -u root -e "SELECT 1;" >/dev/null 2>&1; then
+      break
+    fi
+    sleep 1
+  done
 
-#check db volume if exist if not create new
+  if [ $i -eq 0 ]; then
+    exit 1
+  fi
+
+  mariadb -u root <<EOF
+    CREATE DATABASE $DB_NAME;
+    
+    CREATE USER '$ADMIN_NAME'@'$HOST' IDENTIFIED BY '$ADMIN_PASS_FILE';
+    CREATE USER '$USER_NAME'@'$HOST' IDENTIFIED BY '$USER_PASS_FILE';
+    
+    GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$ADMIN_NAME'@'$HOST';
+    GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'$HOST';
+    
+    FLUSH PRIVILEGES;
+EOF
+
+  # Step 5: Stop the temporary MariaDB daemon.
+  echo "Stopping temporary MariaDB daemon..."
+  kill -TERM $MARIADB_PID
+  wait $MARIADB_PID
+  echo "Setup complete."
+
+else
+  echo "MariaDB data directory already exists. Skipping initial setup."
+fi
+
+# Step 6: Start the main MariaDB daemon for normal operation.
+echo "Starting MariaDB daemon for normal operation."
+exec mariadbd --datadir=$DB_DATA_DIR --user=$USER_NAME
