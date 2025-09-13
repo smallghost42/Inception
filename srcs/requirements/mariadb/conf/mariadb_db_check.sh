b9ad4cec1a38614e3f94b7f308b1d1d6d697ff44
smallghost42 <fenoherymamindrainy@gmail.com>
2025-09-13 08:32:30 +0300

    go

--- srcs/requirements/mariadb/conf/mariadb_db_check.sh
+++ srcs/requirements/mariadb/conf/mariadb_db_check.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-ls $DB_NAME
-if [ $? -ne 0 ]; then
-  mariadb -u root -p $DB_ROOT_PASS_FILE <<EOF
-    CREATE DATABASE $DB_NAME; 
-    CREATE USER '$USER_NAME'@'$HOST' IDENTIFIED BY '$USER_PASS_FILE';
-    GRANT ALL PRIVILEGES ON *.* TO '$USER_NAME'@'$HOST WITH GRANT OPTION; 
-    FLUSH PRIVILEGES;
-EOF
-  mariadb-install-db --user=$USER_NAME --datadir=/app/data
+if [ ! -d "/app/data/mysql" ]; then
+  mariadb-install-db --user=mysql --datadir=/app/data
 fi
+
+mysqld_safe --datadir=/app/data
+sleep 5
+
+DB=$(mariadb -u root -p"$DB_ROOT_PASS_FILE" -e "SHOW DATABASES LIKE '$DB_NAME';" | grep "$DB_NAME" | wc -l)
+
+if [ "$DB" -eq 0 ]; then
+  mariadb -u root -p"$DB_ROOT_PASS_FILE" <<EOF
+CREATE DATABASE $DB_NAME;
+CREATE USER '$USER_NAME'@'$HOST' IDENTIFIED BY '$USER_PASS';
+GRANT ALL PRIVILEGES ON *.* TO '$USER_NAME'@'$HOST' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+fi
+
+mysqladmin -u root -p"$DB_ROOT_PASS_FILE" shutdown
