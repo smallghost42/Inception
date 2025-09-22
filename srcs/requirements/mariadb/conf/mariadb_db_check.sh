c1c464af12056d2848f7d891170c224b95344968
smallghost42 <fenoherymamindrainy@gmail.com>
2025-09-22 13:20:28 +0300

    fix bind_address

--- srcs/requirements/mariadb/conf/mariadb_db_check.sh
+++ srcs/requirements/mariadb/conf/mariadb_db_check.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if [ ! -d "/app/data/mysql" ]; then
-  mariadb-install-db --user=mysql --datadir=/app/data
+  mariadb-install-db --user=mysql --datadir=/app/data &&
+    sed -i 's/^!includedir \/etc\/my\.cnf\.d/#&/' /etc/my.cnf &&
+    sed -i '/^\[mysqld\]/a bind-address=0.0.0.0' /etc/my.cnf
 fi
 
 mariadbd-safe --datadir=/app/data
@@ -11,13 +13,12 @@ DB_ROOT_PASS=$(cat /run/secrets/db_root_pass)
 DB_PASS=$(cat /run/secrets/credentials)
 
 DB=$(mariadb -u root -p"$DB_ROOT_PASS" -e "SHOW DATABASES LIKE '$DB_NAME';" | grep "$DB_NAME" | wc -l)
-sed -i 's/^[[:space:]]*#[[:space:]]*\(bind-address[[:space:]]*=.*\)/\1/' /etc/my.cnf.d/mariadb-server.cnf
 
 if [ "$DB" -eq 0 ]; then
   mariadb -u root -p"$DB_ROOT_PASS" <<EOF
   CREATE DATABASE $DB_NAME;
   CREATE USER '$USER_NAME'@'$HOST' IDENTIFIED BY '$DB_PASS';
-  GRANT ALL PRIVILEGES ON *.* TO '$USER_NAME'@'%' WITH GRANT OPTION;
+  GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%';
   FLUSH PRIVILEGES;
 EOF
 fi
--- srcs/requirements/wordpress/conf/wordpress_db_check.sh
+++ srcs/requirements/wordpress/conf/wordpress_db_check.sh
@@ -4,14 +4,14 @@ chmod -R 755 /app/data
 DB_PASS=$(cat /run/secrets/db_pass)
 ADMIN_PASS=$(cat /run/secrets/credentials2)
 
-if [ ! -f wp-load.php ]; then
+if [ ! -e wp-load.php ]; then
   echo "install core download"
   wp core download --path="/app/data/"
 fi
 
-if [ ! -f wp-config.php ]; then
+if [ ! -e wp-config.php ]; then
   echo "----------------------"
-  sleep 5
+  echo $DB_NAME
   wp config create --dbname="$DB_NAME" --dbuser="$USER_NAME" --dbpass="$DB_PASS" --dbhost="mariadb:3306" --path="/app/data/"
   echo "----------------------"
 fi
